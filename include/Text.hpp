31a3ea67147a76c4954f32bce171df43de330841
DoduCode <itzrealdodu@gmail.com>
2022-10-24 15:19:26 +0530

    Easier to change color, font, and font size

--- include/Text.hpp
+++ include/Text.hpp
@@ -7,15 +7,20 @@
 class Text
 {
     private:
+        const char* fontPath;
+        int fontSize;
         const char* text;
         SDL_Rect textRect;
         SDL_Texture *tex;
 
     public:
-        Text(const char* p_text, int p_x, int p_y, int p_w, int p_h);
+        Text(const char* p_fontPath, int p_fontSize, const char* p_text, std::uint8_t p_r, std::uint8_t p_g, std::uint8_t p_b, int p_x, int p_y, int p_w, int p_h);
 
+        std::uint8_t r, g, b;
         int x, y, w, h;
 
+        const char* getFontPath();
+        int getFontSize();
         const char* getText();
 
         void setText(const char* tval);
--- src/text.cpp
+++ src/text.cpp
@@ -6,9 +6,19 @@
 
 #include "Text.hpp"
 
-Text::Text(const char* p_text, int p_x, int p_y, int p_w, int p_h)
-:text(p_text), x(p_x), y(p_y), w(p_w), h(p_h)
+Text::Text(const char *p_fontPath, int p_fontSize, const char *p_text, uint8_t p_r, uint8_t p_g, uint8_t p_b, int p_x, int p_y, int p_w, int p_h)
+:fontPath(p_fontPath), fontSize(p_fontSize), text(p_text), r(p_r), g(p_g), b(p_b), x(p_x), y(p_y), w(p_w), h(p_h)
 {}
+
+const char* Text::getFontPath()
+{
+    return fontPath;
+}
+
+int Text::getFontSize()
+{
+    return fontSize;
+}
 
 const char* Text::getText()
 {
